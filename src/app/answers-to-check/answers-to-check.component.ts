1caad491a2b1af7124de83fd81f0d1062caf8efc
2ndyrGroupA <2ndyrGroupA@pn.local>
2020-10-29 09:19:00 +0800

    Partial

--- src/app/answers-to-check/answers-to-check.component.ts
+++ src/app/answers-to-check/answers-to-check.component.ts
@@ -8,8 +8,11 @@ import { RequestService } from '../request.service'
   styleUrls: ['./answers-to-check.component.css']
 })
 export class AnswersToCheckComponent implements OnInit {
-  public correct = 0
-  public wrong = 0
+  public correct = []
+  public wrong = []
+  public checkColor = ''
+  public wrongColor = ''
+  public color = false
   public id = ""
   public allData;
   public arrayOfQuestions = []
@@ -31,12 +34,25 @@ export class AnswersToCheckComponent implements OnInit {
       });
     })
   }
-  corrected() {
-    this.correct += 1;
+  corrected(val) {
+    if(!this.wrong.includes(val)) {
+      this.correct.push(val)
+    }else {
+      this.wrong.splice(this.wrong.indexOf(val), 1)
+      if(!this.correct.includes(val)) {
+        this.correct.push(val)
+      }
+    }
   }
-  wronged() {
-    this.wrong +=1;
-
+  wronged(val) {
+    if(!this.correct.includes(val)) {
+      this.wrong.push(val)
+    }else {
+      this.correct.splice(this.correct.indexOf(val), 1)
+      if(!this.wrong.includes(val)) {
+        this.wrong.push(val)
+      }
+    }
   }
 
 
